c5d067c141ff9ba11bb44c8284a7fcaefa00eb09
sawyer tarr <sawyertarr@outlook.com>
2023-02-15 09:06:12 -0600

    GetBalance

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount userBankAccount = new BankAccount();
+
+
+            Console.WriteLine("How much money would you like to deposit?");
+
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            userBankAccount.Deposit(amountToDeposit);
+            double userBalance = userBankAccount.GetBalance();
+
+            Console.WriteLine($"You're new balance is {userBalance,0:c}.");
+
+
+
         }
     }
 }
